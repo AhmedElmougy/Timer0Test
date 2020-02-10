88491e8b7446107bfb6e1a5e7c5b8538b4bbbec0
Ahmed Elmogy <52128898+AhmedElmougy@users.noreply.github.com>
2020-02-10 15:11:43 +0200

    debug files

---
+++ Debug/makedep.mk
@@ -0,0 +1,12 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+HLED_Program.c
+
+main.c
+
+MDIO_Program.c
+
+MTIMER0_Program.c
+
